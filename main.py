24f81aa9a6bb7183be3a9bc0f696adb724fca821
Rezapyon <rezapyon@gmail.com>
2025-02-16 20:44:40 +0700

    update file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -7,21 +7,21 @@ from sklearn.metrics.pairwise import cosine_similarity
 def get_recommendations(group_type, user_interest, data, result_count=5):
     user_input = user_interest + '#' + group_type
 
-    user_df = pd.DataFrame({'combined': [user_input]})
-    combined_df = pd.concat([data['combined'], user_df['combined']], ignore_index=True)
+    user_df = pandas.DataFrame({'combined': [user_input]})
+    combined_df = pandas.concat([data['combined'], user_df['combined']], ignore_index=True)
 
-    tfidf_vectorizer = TfidfVectorizer()
-    tfidf_matrix = tfidf_vectorizer.fit_transform(combined_df)
+    tf_idf_vector = TfidfVectorizer()
+    tf_idf_mx = tf_idf_vector.fit_transform(combined_df)
 
-    cosine_sim = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1])
+    similar_cosine = cosine_similarity(tf_idf_mx[-1], tf_idf_mx[:-1])
 
-    sim_scores = list(enumerate(cosine_sim[0]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:result_count + 1]
+    similar_scores = list(enumerate(similar_cosine[0]))
+    similar_scores = sorted(similar_scores, key=lambda x: x[1], reverse=True)
+    similar_scores = similar_scores[1:result_count + 1]
 
-    recommended_indices = [i[0] for i in sim_scores]
+    recom_index = [i[0] for i in similar_scores]
 
-    recommendations = data.iloc[recommended_indices].copy()
+    recommendations = data.iloc[recom_index].copy()
     recommendations['no'] = range(1, len(recommendations) + 1)
     return recommendations[['no', 'nama', 'deskripsi', 'alamat']]
 
@@ -31,15 +31,15 @@ if __name__ == "__main__":
     user_interest = "Tempat unik untuk berpetualang"
     result_count = 5
 
-    data = pd.read_csv('yogyakarta.csv', delimiter=',')
+    frameYogya = pandas.read_csv('yogyakarta.csv', delimiter=',')
 
-    data['combined'] = data['deskripsi'] + '#' + data['kategori'] + '#' + data['group']
+    frameYogya['combined'] = frameYogya['deskripsi'] + '#' + frameYogya['kategori'] + '#' + frameYogya['group']
 
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.expand_frame_repr', False)
-    pd.set_option('display.max_colwidth', None)
+    pandas.set_option('display.max_columns', None)
+    pandas.set_option('display.expand_frame_repr', False)
+    pandas.set_option('display.max_colwidth', None)
 
-    recommendations = get_recommendations(group_type, user_interest, data, result_count)
+    recommendations = get_recommendations(group_type, user_interest, frameYogya, result_count)
 
     print("\nRekomendasi tempat wisata:")
     print(recommendations)
